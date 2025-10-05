7f50ac8d0c7808741d5b5623a4f9138648a1986f
Mesud Mujanovic <mesud9494@gmail.com>
2025-10-05 21:08:28 +0200

    Initial commit: full project with backend and frontend

---
+++ chshop/src/main/java/com/chsoph/ChshopApplication.java
@@ -0,0 +1,13 @@
+package com.chsoph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChshopApplication.class, args);
+	}
+
+}
---
+++ chshop/src/test/java/com/chsoph/ChshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chsoph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

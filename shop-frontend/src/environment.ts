552be95930a8d5a1f1d18085c2e00adcb8dff2ed
Mesud Mujanovic <mesud9494@gmail.com>
2025-10-19 18:18:48 +0200

    "Work space"

--- shop-frontend/src/environment.ts
+++ shop-frontend/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  stripePublishableKey: '',
+  stripePublishableKey: 'pk_test_51SJvtnDQ2b4ApNu3hNQpjnOy7opsl6lqMVcJ2HV35dakJItPdvmU3texP3534sv7QGihOakTqKPcMN78CfXwFKim00mTZ72iLg',
   apiUrl: 'http://localhost:8080/api'
 };

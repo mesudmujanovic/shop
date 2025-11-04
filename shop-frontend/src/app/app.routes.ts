b3c536ed092c4e0b5b67e4e882eaf3063ea66123
Mesud Mujanovic <mesud9494@gmail.com>
2025-11-04 22:12:34 +0100

    "Work space"

--- shop-frontend/src/app/app.routes.ts
+++ shop-frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
 import { PaymentComponent } from './payment/payment.component';
+import { SingleProductComponent } from './product/single-product/single-product.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: SingleProductComponent },
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: PaymentComponent },
   { path: '**', redirectTo: '/products' }
